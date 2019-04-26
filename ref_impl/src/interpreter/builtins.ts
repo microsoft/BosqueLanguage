e3dcc853b365a3593d235346d71ad6a760a06ca9
Sebastian Kaczmarek <skaczmarek03@gmail.com>
2019-04-26 23:33:55 +0200

    Add List::findLast method

--- ref_impl/src/interpreter/builtins.ts
+++ ref_impl/src/interpreter/builtins.ts
@@ -198,6 +198,13 @@ const BuiltinCalls = new Map<string, BuiltinCallSig>()
         raiseRuntimeErrorIf(idx === -1);
         return (args.get("this") as ListValue).values[idx];
     })
+    .set("list_findLast", (ep: InterpreterEntryPoint, inv: MIRInvokeDecl, masm: MIRAssembly, args: Map<string, Value>): Value => {
+        const lambda = args.get("p") as LambdaValue;
+        const idx = (args.get("this") as ListValue).values.reverse().findIndex((v) => ValueOps.convertBoolOrNoneToBool(ep(lambda, [v])));
+
+        raiseRuntimeErrorIf(idx === -1);
+        return (args.get("this") as ListValue).values[idx];
+    })
     .set("list_tryfind", (ep: InterpreterEntryPoint, inv: MIRInvokeDecl, masm: MIRAssembly, args: Map<string, Value>): Value => {
         const lambda = args.get("p") as LambdaValue;
         const idx = (args.get("this") as ListValue).values.findIndex((v) => ValueOps.convertBoolOrNoneToBool(ep(lambda, [v])));
