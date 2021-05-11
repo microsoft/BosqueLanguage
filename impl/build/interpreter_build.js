54bec90d1d170397b66d1bdf82dd1bdceb150062
Mark Marron <marron@microsoft.com>
2021-05-10 22:33:51 -0700

    build

--- impl/build/interpreter_build.js
+++ impl/build/interpreter_build.js
@@ -3,13 +3,35 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-const fs = require(fs);
+const fs = require("fs");
 const path = require("path");
-const fsx = require("fs-extra");
 
 const rootsrc = path.normalize(path.join(__dirname, "../", "src/tooling/interpreter/"));
 const alldirs = [rootsrc, path.join(rootsrc, "assembly"), path.join(rootsrc, "core"), path.join(rootsrc, "runtime")];
+const cppfiles = [].concat(...alldirs.map((dd) => fs.readdirSync(dd).filter(fn => fn.endsWith('.cpp'))));
 
-var cppfiles = [].join(...alldirs.forEach((dd) => fs.readdirSync(dd).filter(fn => fn.endsWith('.cpp'))));
+console.log(cppfiles);
 
-//const rootbin = path.normalize(path.join(__dirname, "../", "bin"));
+const ccflags = "-g -BSQ_DEBUG_BUILD -std=c++17";  
+
+let compiler = "";
+let includes = " ";
+let outfile = "";
+if(process.platform === "darwin") {
+    compiler = "clang++";
+    outfile = path.normalize(__dirname, "tools/macos/icpp");
+}
+else if(process.platform === "linux") {
+    compiler = "clang++";
+    outfile = path.normalize(__dirname, "tools/linux/icpp");
+}
+else {
+    compiler = "clang.exe";
+    includes = " --include-directory=\"C:\\Program Files\\boost_1_76_0\" --library-directory=\"C:\\Program Files\\boost_1_76_0\\stage\\lib\" ";
+    outfile = path.normalize(__dirname, "tools/win/icpp.exe");
+}
+
+const command = `${compiler} ${ccflags}${includes} -o ${outfile} ${cppfiles.join(" ")}`;
+
+console.log("Running compile:");
+console.log(command);
