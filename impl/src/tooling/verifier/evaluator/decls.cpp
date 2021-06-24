9b9a2af9870c6efd325f1a5a5e4d2f5e3ce48ec9
Mark Marron <marron@microsoft.com>
2021-06-23 17:19:12 -0700

    working

--- impl/src/tooling/verifier/evaluator/decls.cpp
+++ impl/src/tooling/verifier/evaluator/decls.cpp
@@ -5,6 +5,369 @@
 
 #include "decls.h"
 
+void ExtractionInfo::loadConsFuncs(std::map<std::string, std::optional<z3::func_decl>>& consfuncs)
+{
+    xxx;
+}
+
+bool FuzzInfo::hasConstantsForType(TypeTag tag) const
+{
+    return this->constants.find(tag) != this->constants.cend();
+}
+
+void FuzzInfo::addConstantForType(TypeTag tag, json j)
+{
+    if(!this->hasConstantsForType(tag))
+    {
+        this->constants[tag] = {};
+    }
+
+    this->constants[tag].push_back({j.dump(), j});
+}
+
+json FuzzInfo::pickConstantForType(TypeTag tag, RandGenerator& rnd) const
+{
+    auto citer = this->constants.find(tag);
+
+    std::uniform_int_distribution<size_t> cdist(0, citer->second.size() - 1);
+    return citer->second.at(cdist(rnd));
+}
+
+bool FuzzInfo::hasReuseForType(TypeTag tag) const
+{
+    return this->reuse.find(tag) != this->reuse.cend();
+}
+
+void FuzzInfo::addReuseForType(TypeTag tag, json j)
+{
+    if(!this->hasReuseForType(tag))
+    {
+        this->reuse[tag] = {};
+    }
+    std::vector<std::pair<std::string, json>>& tve = this->reuse[tag];
+
+    std::string str = j.dump();
+    auto mval = std::find_if(tve.cbegin(), tve.cend(), [&str](const std::pair<std::string, json>& entry) {
+        entry.first == str;
+    });
+
+    if(mval == tve.cend())
+    {
+        tve.push_back({j.dump(), j});
+    }
+}
+
+json FuzzInfo::pickReuseForType(TypeTag tag, RandGenerator& rnd) const
+{
+    auto riter = this->reuse.find(tag);
+
+    std::uniform_int_distribution<size_t> rdist(0, riter->second.size() - 1);
+    return riter->second.at(rdist(rnd));
+}
+
+bool FuzzInfo::sampleKnownOpt(TypeTag tag, RandGenerator& rnd, json& j)
+{
+    std::uniform_int_distribution<size_t> fchoice(0, 3);
+    auto cval = fchoice(rnd);
+
+    if(cval == 0 || cval == 1)
+    {
+        return false;
+    }
+    else if(cval == 2)
+    {
+        if(!this->hasConstantsForType(tag))
+        {
+            return false;
+        }
+        else
+        {
+            j = this->pickConstantForType(tag, rnd);
+            return true;
+        }
+    }
+    else
+    {
+         if(!this->hasReuseForType(tag))
+        {
+            return false;
+        }
+        else
+        {
+            j = this->pickReuseForType(tag, rnd);
+            return true;
+        }
+    }
+}
+
+std::string generateRandomChar(RandGenerator& rnd)
+{
+    constexpr size_t printascii = 94;
+    constexpr size_t nullchar = 1; 
+    constexpr size_t smallunicode = 2; 
+    constexpr size_t bigunicode = 1; 
+
+    std::uniform_int_distribution<size_t> cchoice(0, printascii + nullchar + smallunicode + bigunicode);
+    auto cval = cchoice(rnd);
+
+    uint8_t small_unic[] = {/*£*/ 194, 163, /*µ*/ 194, 181};
+    uint8_t emoji[] = {/*🌵*/ 240, 159, 140, 181};
+    
+
+    std::string data;
+    if(cval <= printascii)
+    {
+        data.append({ (char)(32 + cval) });
+    }
+    else if(cval == printascii + nullchar)
+    {
+        data.append('\0');
+    }
+    else
+    {
+        if(cval <= printascii + nullchar + smallunicode)
+        {
+            size_t pos = 2*(cval - (printascii + nullchar));
+            data.append({ (char)small_unic[pos], (char)small_unic[pos + 1] });
+        }
+        else
+        {
+            data.append({ (char)emoji[0], (char)emoji[1],  (char)emoji[2],  (char)emoji[3] });
+        }
+    }
+
+    return data;
+}
+
+BSQRegexOpt* BSQRegexOpt::parse(json j)
+{
+    if(j.is_string())
+    {
+        return BSQLiteralRe::parse(j);
+    }
+    else
+    {
+        auto tag = j["tag"].get<std::string>();
+        if(tag == "CharClass")
+        {
+            return BSQCharClassRe::parse(j);
+        }
+        else if(tag == "CharRange")
+        {
+            return BSQCharRangeRe::parse(j);
+        }
+        else if(tag == "StarRepeat")
+        {
+            return BSQStarRepeatRe::parse(j);
+        }
+        else if(tag == "PlusRepeat")
+        {
+            return BSQPlusRepeatRe::parse(j);
+        }
+        else if(tag == "RangeRepeat")
+        {
+            return BSQRangeRepeatRe::parse(j);
+        }
+        else if(tag == "Optional")
+        {
+            return BSQOptionalRe::parse(j);
+        }
+        else if(tag == "Alternation")
+        {
+            return BSQAlternationRe::parse(j);
+        }
+        else
+        {
+            return BSQSequenceRe::parse(j);
+        }
+    }
+}
+
+std::string BSQLiteralRe::generate(RandGenerator& rnd, FuzzInfo& finfo) const
+{
+    return this->litval;
+}
+
+BSQLiteralRe* BSQLiteralRe::parse(json j)
+{
+    return new BSQLiteralRe(j.get<std::string>());
+}
+
+std::string BSQCharRangeRe::generate(RandGenerator& rnd, FuzzInfo& finfo) const
+{
+    //
+    //TODO: unicode support and escape support
+    //
+    assert(32 <= low && high < 126);
+
+    std::uniform_int_distribution<uint32_t> distribution(low, high);
+    return std::string{(char)distribution(rnd)};
+}
+
+BSQCharRangeRe* BSQCharRangeRe::parse(json j)
+{
+    auto lb = j["lb"].get<uint64_t>();
+    auto ub = j["ub"].get<uint64_t>();
+
+    return new BSQCharRangeRe(lb, ub);
+}
+
+std::string BSQCharClassRe::generate(RandGenerator& rnd, FuzzInfo& finfo) const
+{
+    //
+    //TODO: unicode support and escape support
+    //
+    if(this->kind == SpecialCharKind::Wildcard)
+    {
+        return generateRandomChar(rnd);
+    }
+    else
+    {
+        char ws_char[] = {' ', '\n', '\r', '\t' };
+        std::uniform_int_distribution<uint32_t> chardist(0, sizeof(ws_char) - 1);
+
+        return std::string{ws_char[chardist(rnd)]};
+    }
+}
+
+BSQCharClassRe* BSQCharClassRe::parse(json j)
+{
+    auto kind = j["kind"].get<SpecialCharKind>();
+
+    return new BSQCharClassRe(kind);
+}
+
+std::string BSQStarRepeatRe::generate(RandGenerator& rnd, FuzzInfo& finfo) const
+{
+    auto ulimc = finfo.small_model_gen ? 3 : 10;
+    std::uniform_int_distribution<size_t> ctdist(0, ulimc);
+
+    auto ct = ctdist(rnd);
+    std::string res;
+    for(size_t i = 0; i < ct; ++i)
+    {
+        res.append(this->opt->generate(rnd, finfo));
+    }
+
+    return res;
+}
+
+BSQStarRepeatRe* BSQStarRepeatRe::parse(json j)
+{
+    auto repeat = BSQRegexOpt::parse(j["repeat"]);
+
+    return new BSQStarRepeatRe(repeat);
+}
+
+std::string BSQPlusRepeatRe::generate(RandGenerator& rnd, FuzzInfo& finfo) const
+{
+    auto ulimc = finfo.small_model_gen ? 3 : 10;
+    std::uniform_int_distribution<size_t> ctdist(1, ulimc);
+
+    auto ct = ctdist(rnd);
+    std::string res;
+    for(size_t i = 0; i < ct; ++i)
+    {
+        res.append(this->opt->generate(rnd, finfo));
+    }
+
+    return res;
+}
+
+BSQPlusRepeatRe* BSQPlusRepeatRe::parse(json j)
+{
+    auto repeat = BSQRegexOpt::parse(j["repeat"]);
+
+    return new BSQPlusRepeatRe(repeat);
+}
+
+std::string BSQRangeRepeatRe::generate(RandGenerator& rnd, FuzzInfo& finfo) const
+{
+    std::uniform_int_distribution<size_t> ctdist(this->low, this->high);
+
+    auto ct = ctdist(rnd);
+    std::string res;
+    for(size_t i = 0; i < ct; ++i)
+    {
+        res.append(this->opt->generate(rnd, finfo));
+    }
+
+    return res;
+}
+
+BSQRangeRepeatRe* BSQRangeRepeatRe::parse(json j)
+{
+    auto min = j["min"].get<uint64_t>();
+    auto max = j["max"].get<uint64_t>();
+    auto repeat = BSQRegexOpt::parse(j["repeat"]);
+
+    return new BSQRangeRepeatRe(min, max, repeat);
+}
+
+std::string BSQOptionalRe::generate(RandGenerator& rnd, FuzzInfo& finfo) const
+{
+    std::uniform_int_distribution<uint32_t> ctdist(0, 1);
+
+    auto ct = ctdist(rnd);
+    if(ct == 1)
+    {
+        return this->opt->generate(rnd, finfo);
+    }
+    else
+    {
+        return "";
+    }
+}
+
+BSQOptionalRe* BSQOptionalRe::parse(json j)
+{
+    auto opt = BSQRegexOpt::parse(j["opt"]);
+
+    return new BSQOptionalRe(opt);
+}
+
+std::string BSQAlternationRe::generate(RandGenerator& rnd, FuzzInfo& finfo) const
+{
+    std::uniform_int_distribution<size_t> idxdist(0, this->opts.size() - 1);
+    auto opt = this->opts[idxdist(rnd)];
+
+    return opt->generate(rnd, finfo);   
+}
+
+BSQAlternationRe* BSQAlternationRe::parse(json j)
+{
+    std::vector<const BSQRegexOpt*> opts;
+    auto jopts = j["opts"];
+    std::transform(jopts.cbegin(), jopts.cend(), std::back_inserter(opts), [](json arg) {
+        return BSQRegexOpt::parse(arg);
+    });
+
+    return new BSQAlternationRe(opts);
+}
+
+std::string BSQSequenceRe::generate(RandGenerator& rnd, FuzzInfo& finfo) const
+{
+    std::string res;
+    for(size_t i = 0; i < this->opts.size(); ++i)
+    {
+        res.append(this->opts[i]->generate(rnd, finfo));
+    }
+
+    return res;
+}
+
+BSQSequenceRe* BSQSequenceRe::parse(json j)
+{
+    std::vector<const BSQRegexOpt*> elems;
+    auto jelems = j["elems"];
+    std::transform(jelems.cbegin(), jelems.cend(), std::back_inserter(elems), [](json arg) {
+        return BSQRegexOpt::parse(arg);
+    });
+
+    return new BSQSequenceRe(elems);
+}
+
+
 IType* IType::jparse(json j)
 {
     xxxx;
@@ -12,10 +375,10 @@ IType* IType::jparse(json j)
 
 NoneType* NoneType::jparse(json j)
 {
-    return new NoneType("NSCore::None");
+    return new NoneType();
 }
 
-json NoneType::fuzz(RandGenerator& rnd) const
+json NoneType::fuzz(FuzzInfo& finfo, RandGenerator& rnd) const
 {
     return nullptr;
 }
@@ -28,4 +391,93 @@ json NoneType::extract(ExtractionInfo* ex, z3::expr ctx) const
 std::string NoneType::consprint(json j) const
 {
     return "none";
+}
+
+BoolType* BoolType::jparse(json j)
+{
+    return new BoolType();
+}
+
+json BoolType::fuzz(FuzzInfo& finfo, RandGenerator& rnd) const
+{
+    std::uniform_int_distribution<int> distribution(0, 1);
+    return distribution(rnd) == 1 ? true : false;
+}
+
+json BoolType::extract(ExtractionInfo* ex, z3::expr ctx) const
+{
+    auto v = ex->evalConsFunc("NSCore::Bool", ctx);
+    assert(v.is_bool() && (v.is_true() || v.is_false()));
+
+    return v.is_true();
+}
+
+std::string BoolType::consprint(json j) const
+{
+    return j.get<bool>() ? "true" : "false";
+}
+
+NatType* NatType::jparse(json j)
+{
+    return new NatType();
+}
+
+json NatType::fuzz(FuzzInfo& finfo, RandGenerator& rnd) const
+{
+    json res;
+    if(finfo.sampleKnownOpt(TypeTag::NatTag, rnd, res))
+    {
+        return res;
+    }
+    else
+    {
+        auto ub = finfo.small_model_gen ? 10 : finfo.limits.nat_max;
+    
+        std::uniform_int_distribution<uint64_t> distribution(0, ub);
+        res = std::to_string(distribution(rnd));
+
+        finfo.addReuseForType(TypeTag::NatTag, res);
+        return res;
+    }
+}
+
+json NatType::extract(ExtractionInfo* ex, z3::expr ctx) const
+{
+    auto v = ex->evalConsFunc("NSCore::Nat", ctx);
+    assert(v.is_bv());
+
+    return v.();
+}
+
+std::string NatType::consprint(json j) const
+{
+    return  j.get<std::string>() + "n";
+}
+
+IntType* IntType::jparse(json j)
+{
+    return new IntType();
+}
+
+json IntType::fuzz(FuzzInfo& finfo, RandGenerator& rnd) const
+{
+    xxxx;
+    auto lb = finfo.small_model_gen ? -10 : finfo.limits.int_min;
+    auto ub = finfo.small_model_gen ? 10 : finfo.limits.int_max;
+    
+    std::uniform_int_distribution<uint64_t> distribution(lb, ub);
+    return std::to_string(distribution(rnd));
+}
+
+json IntType::extract(ExtractionInfo* ex, z3::expr ctx) const
+{
+    auto v = ex->evalConsFunc("NSCore::Int", ctx);
+    assert(v.is_bv());
+
+    return v.();
+}
+
+std::string IntType::consprint(json j) const
+{
+    return j.get<std::string>() + "i";
 }
--- impl/src/tooling/verifier/evaluator/decls.h
+++ impl/src/tooling/verifier/evaluator/decls.h
@@ -15,7 +15,10 @@
 #include <random>
 typedef std::default_random_engine RandGenerator;
 
+#include "json.hpp"
 #include "z3++.h"
+
+typedef nlohmann::json json;
 
 enum class TypeTag
 {
@@ -41,11 +44,215 @@ enum class TypeTag
     ContentHashTag
 };
 
-class ExtractionInfo
+struct NumericLimits
+{
+    uint64_t nat_max;
+    int64_t int_min;
+    int64_t int_max;
+};
+
+class FuzzInfo
 {
 public:
-    z3::model m;
+    const bool small_model_gen;
+    const NumericLimits limits;
+
+    std::map<TypeTag, std::vector<std::pair<std::string, json>>> constants;
+    std::map<TypeTag, std::vector<std::pair<std::string, json>>> reuse;
+
+    bool hasConstantsForType(TypeTag tag) const;
+    void addConstantForType(TypeTag tag, json j);
+    json pickConstantForType(TypeTag tag, RandGenerator& rnd) const;
+
+    bool hasReuseForType(TypeTag tag) const;
+    void addReuseForType(TypeTag tag, json j);
+    json pickReuseForType(TypeTag tag, RandGenerator& rnd) const;
+
+    bool sampleKnownOpt(TypeTag tag, RandGenerator& rnd, json& j);
+
+    FuzzInfo(bool small, NumericLimits limits) : small_model_gen(small), limits(limits) {;}
+};
+
+class ExtractionInfo
+{
+private:
+    static void loadConsFuncs(std::map<std::string, std::optional<z3::func_decl>>& consfuncs);
+
+public:
+    z3::context* c;
+    z3::model* m;
     std::map<std::string, std::optional<z3::func_decl>> consfuncs;
+
+    ExtractionInfo(z3::context* c, z3::model* m) : c(c), m(m)
+    {
+        ExtractionInfo::loadConsFuncs(this->consfuncs);
+    }
+
+    z3::expr evalConsFunc(const char* cf, z3::expr ctx)
+    {
+        auto valexp = this->consfuncs[cf].value()(ctx);
+        auto resexp = this->m->eval(valexp, true);
+
+        return resexp;
+    }
+
+    z3::expr advanceCtx(z3::expr ctx)
+    {
+        z3::concat(ctx, this->consfuncs["MakeStep"].value()(this->c->bv_val(0, 5)));
+    }
+};
+
+////
+//Regex
+
+enum class SpecialCharKind 
+{
+    Wildcard = 0x0,
+    WhiteSpace
+};
+
+class BSQRegexOpt
+{
+public:
+    BSQRegexOpt() {;}
+    virtual ~BSQRegexOpt() {;}
+
+    virtual std::string generate(RandGenerator& rnd, FuzzInfo& finfo) const = 0;
+
+    static BSQRegexOpt* parse(json j);
+};
+
+class BSQLiteralRe : public BSQRegexOpt
+{
+public:
+    const std::string litval;
+
+    BSQLiteralRe(std::string lv) : BSQRegexOpt(), litval(lv) {;}
+    virtual ~BSQLiteralRe() {;}
+
+    virtual std::string generate(RandGenerator& rnd, FuzzInfo& finfo) const override final;
+
+    static BSQLiteralRe* parse(json j);
+};
+
+class BSQCharRangeRe : public BSQRegexOpt
+{
+public:
+    const uint64_t low;
+    const uint64_t high;
+
+    BSQCharRangeRe(uint64_t low, uint64_t high) : BSQRegexOpt(), low(low), high(high) {;}
+    virtual ~BSQCharRangeRe() {;}
+
+    virtual std::string generate(RandGenerator& rnd, FuzzInfo& finfo) const override final;
+
+    static BSQCharRangeRe* parse(json j);
+};
+
+class BSQCharClassRe : public BSQRegexOpt
+{
+public:
+    const SpecialCharKind kind;
+
+    BSQCharClassRe(SpecialCharKind kind) : BSQRegexOpt(), kind(kind) {;}
+    virtual ~BSQCharClassRe() {;}
+
+    virtual std::string generate(RandGenerator& rnd, FuzzInfo& finfo) const override final;
+
+    static BSQCharClassRe* parse(json j);
+};
+
+class BSQStarRepeatRe : public BSQRegexOpt
+{
+public:
+    const BSQRegexOpt* opt;
+
+    BSQStarRepeatRe(const BSQRegexOpt* opt) : BSQRegexOpt(), opt(opt) {;}
+    virtual ~BSQStarRepeatRe() {;}
+
+    virtual std::string generate(RandGenerator& rnd, FuzzInfo& finfo) const override final;
+
+    static BSQStarRepeatRe* parse(json j);
+};
+
+class BSQPlusRepeatRe : public BSQRegexOpt
+{
+public:
+    const BSQRegexOpt* opt;
+
+    BSQPlusRepeatRe(const BSQRegexOpt* opt) : BSQRegexOpt(), opt(opt) {;}
+    virtual ~BSQPlusRepeatRe() {;}
+
+    virtual std::string generate(RandGenerator& rnd, FuzzInfo& finfo) const override final;
+
+    static BSQPlusRepeatRe* parse(json j);
+};
+
+class BSQRangeRepeatRe : public BSQRegexOpt
+{
+public:
+    const uint32_t low;
+    const uint32_t high;
+    const BSQRegexOpt* opt;
+
+    BSQRangeRepeatRe(uint32_t low, uint32_t high, const BSQRegexOpt* opt) : BSQRegexOpt(), opt(opt), low(low), high(high) {;}
+    virtual ~BSQRangeRepeatRe() {;}
+
+    virtual std::string generate(RandGenerator& rnd, FuzzInfo& finfo) const override final;
+
+    static BSQRangeRepeatRe* parse(json j);
+};
+
+class BSQOptionalRe : public BSQRegexOpt
+{
+public:
+    const BSQRegexOpt* opt;
+
+    BSQOptionalRe(const BSQRegexOpt* opt) : BSQRegexOpt(), opt(opt) {;}
+    virtual ~BSQOptionalRe() {;}
+
+    virtual std::string generate(RandGenerator& rnd, FuzzInfo& finfo) const override final;
+
+    static BSQOptionalRe* parse(json j);
+};
+
+class BSQAlternationRe : public BSQRegexOpt
+{
+public:
+    const std::vector<const BSQRegexOpt*> opts;
+
+    BSQAlternationRe(std::vector<const BSQRegexOpt*> opts) : BSQRegexOpt(), opts(opts) {;}
+    virtual ~BSQAlternationRe() {;}
+
+    virtual std::string generate(RandGenerator& rnd, FuzzInfo& finfo) const override final;
+
+    static BSQAlternationRe* parse(json j);
+};
+
+class BSQSequenceRe : public BSQRegexOpt
+{
+public:
+    const std::vector<const BSQRegexOpt*> opts;
+
+    BSQSequenceRe(std::vector<const BSQRegexOpt*> opts) : BSQRegexOpt(), opts(opts) {;}
+    virtual ~BSQSequenceRe() {;}
+
+    virtual std::string generate(RandGenerator& rnd, FuzzInfo& finfo) const override final;
+
+    static BSQSequenceRe* parse(json j);
+};
+
+struct BSQRegex
+{
+    std::string restr;
+    bool isAnchorStart;
+    bool isAnchorEnd;
+    BSQRegexOpt* re;
+
+    std::string generate(RandGenerator& rnd, FuzzInfo& finfo)
+    {
+        return this->re->generate(rnd, finfo);
+    }  
 };
 
 class IType
@@ -58,7 +265,7 @@ public:
 
     static IType* jparse(json j);
 
-    virtual json fuzz(RandGenerator& rnd) const = 0;
+    virtual json fuzz(FuzzInfo& finfo, RandGenerator& rnd) const = 0;
     virtual json extract(ExtractionInfo* ex, z3::expr ctx) const = 0;
 
     virtual std::string consprint(json j) const = 0;
@@ -67,12 +274,12 @@ public:
 class NoneType : public IType
 {
 public:
-    NoneType(std::string name) : IType(name) {;}
+    NoneType() : IType("NSCore::None") {;}
     virtual ~NoneType() {;}
 
     static NoneType* jparse(json j);
 
-    virtual json fuzz(RandGenerator& rnd) const override final;
+    virtual json fuzz(FuzzInfo& finfo, RandGenerator& rnd) const override final;
     virtual json extract(ExtractionInfo* ex, z3::expr ctx) const override final;
 
     virtual std::string consprint(json j) const override final;
@@ -81,10 +288,12 @@ public:
 class BoolType : public IType
 {
 public:
-    BoolType(std::string name) : IType(name) {;}
+    BoolType() : IType("NSCore::Bool") {;}
     virtual ~BoolType() {;}
 
-    virtual json fuzz(RandGenerator& rnd) const override final;
+    static BoolType* jparse(json j);
+
+    virtual json fuzz(FuzzInfo& finfo, RandGenerator& rnd) const override final;
     virtual json extract(ExtractionInfo* ex, z3::expr ctx) const override final;
 
     virtual std::string consprint(json j) const override final;
@@ -93,10 +302,12 @@ public:
 class NatType : public IType
 {
 public:
-    NatType(std::string name) : IType(name) {;}
+    NatType() : IType("NSCore::Nat") {;}
     virtual ~NatType() {;}
 
-    virtual json fuzz(RandGenerator& rnd) const override final;
+    static NatType* jparse(json j);
+
+    virtual json fuzz(FuzzInfo& finfo, RandGenerator& rnd) const override final;
     virtual json extract(ExtractionInfo* ex, z3::expr ctx) const override final;
 
     virtual std::string consprint(json j) const override final;
@@ -104,17 +315,40 @@ public:
 
 class IntType : public IType
 {
+public:
+    IntType() : IType("NSCore::Int") {;}
+    virtual ~IntType() {;}
 
+    static IntType* jparse(json j);
+
+    virtual json fuzz(FuzzInfo& finfo, RandGenerator& rnd) const override final;
+    virtual json extract(ExtractionInfo* ex, z3::expr ctx) const override final;
+
+    virtual std::string consprint(json j) const override final;
 };
 
 class BigNatType : public IType
 {
+public:
+    BigNatType() : IType("NSCore::BigNat") {;}
+    virtual ~BigNatType() {;}
 
+    virtual json fuzz(FuzzInfo& finfo, RandGenerator& rnd) const override final;
+    virtual json extract(ExtractionInfo* ex, z3::expr ctx) const override final;
+
+    virtual std::string consprint(json j) const override final;
 };
 
 class BigIntType : public IType
 {
+public:
+    BigIntType() : IType("NSCore::BigInt") {;}
+    virtual ~BigIntType() {;}
 
+    virtual json fuzz(FuzzInfo& finfo, RandGenerator& rnd) const override final;
+    virtual json extract(ExtractionInfo* ex, z3::expr ctx) const override final;
+
+    virtual std::string consprint(json j) const override final;
 };
 
 class RationalType : public IType
