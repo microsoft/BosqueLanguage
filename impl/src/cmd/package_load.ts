d524f225ef9b68dc15e5f97f13fc019ef1bbd510
Mark Marron <marron@microsoft.com>
2022-03-10 17:30:51 -0800

    packaging

--- impl/src/cmd/package_load.ts
+++ impl/src/cmd/package_load.ts
@@ -3,21 +3,21 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-type URIPathString = string; //path parts not things like sep or prefix
-
 type URIPath = {
     scheme: string,
-    authority: [URIPathString],
-    path: [URIPathString],
-    extension: string;
+    authority: string | undefined,
+    port: number | undefined,
+    path: string,
+    extension: string | undefined;
 };
 
 type URIPathGlob = {
     scheme: string,
-    authority: [URIPathString],
-    path: [URIPathString],
+    authority: string | undefined,
+    port: number | undefined,
+    path: string,
     selection: "*" | "**",
-    filter: string
+    filter: string | undefined
 }
 
 type Version = {
@@ -25,26 +25,55 @@ type Version = {
     minor: number,
     fix: number,
     patch: number,
-    branch: URIPathString
+    branch: string | undefined
 };
+
+type PackageFormat = "Inline" | "Component" | "Service";
 
 type Contact = {
     name: string,
     role: string,
-    email: string,
-    url: URIPath
+    email: string | undefined,
+    url: URIPath | undefined
 };
 
 type SourceInfo = {
     bsqsource: URIPathGlob[],
     entrypoints: URIPathGlob[],
     testfiles: URIPathGlob[]
-}
+};
+
+type CompilerOptions = {
+};
+
+type CheckerOptions = {
+    INT_MIN: number,
+    INT_MAX: number,
+    SLEN_MAX: number,
+    BLEN_MAX: number,
+
+    CONTAINER_MAX: number
+};
+
+type Config = {
+    macros: string[],
+    globalmacros: string[],
+    buildlevel: "debug" | "test" | "release",
+    testbuild: boolean,
+    options: CompilerOptions | CheckerOptions
+};
+
+type Dependency = {
+    name: string,
+    version: Version,
+    internalOnly: boolean
+};
 
 //exports must include scope component of the form `${name}${version.major}`
 type Package = {
-    name: URIPathString;
+    name: string;
     version: Version,
+    format: PackageFormat,
     description: string
     keywords: string[],
     homepage: URIPath,
@@ -57,8 +86,80 @@ type Package = {
         files: URIPath[],
         root: URIPath
     },
-    configs: xxxx,
-    dependencies: xxxx,
+    configs: Config[],
+    dependencies: Dependency[],
+    devDependencies: Dependency[]
 };
 
+function parseURIScheme(str: string): [string | undefined, string] {
+    if (process.platform === "win32") {
+        if (/^[a-zA-Z]:\\/.test(str) || str.startsWith(".\\") || str.startsWith("..\\")) {
+            return ["file", str];
+        }
+    }
+    
+    if ((process.platform === "linux" || process.platform === "darwin")) {
+        if (str.startsWith("/") || str.startsWith("~/") || str.startsWith("./") || str.startsWith("../")) {
+            return ["file", str];
+        }
+    }
+
+    const schemematch = /:/.exec(str);
+    if (schemematch !== null) {
+        return [schemematch[0].slice(0, schemematch.index), str.slice(schemematch.index + 1)];
+    }
+    else {
+        return [undefined, str];
+    }
+}
+
+function parseURIAuthority(str: string): [string | undefined, number | undefined, string] {
+    if(!str.startsWith("//")) {
+        return [undefined, undefined, str];
+    }
+
+    const authoritymatch = /^\/\/[a-zA-Z0-9.]+/.exec(str);
+    if (authoritymatch !== null) {
+        const tail = str.slice(authoritymatch[0].length + 1);
+        const port = /^:[0-9]{1, 5}/.exec(tail);
+        if(port === null) {
+            return [authoritymatch[0].slice(2, authoritymatch[0].length), undefined, tail];
+        }
+        else {
+            const tail2 = tail.slice(port[0].length + 1);
+            const portval = Number.parseInt(port[0].slice(1));
+
+            return [authoritymatch[0].slice(2, authoritymatch[0].length), portval, tail2]
+        }
+    }
+    else {
+        return [undefined, undefined, str];
+    }
+}
+
+function parseURIPathBase(str: string): {scheme: string, authority: string | undefined, port: number | undefined, path: string, extension: string | undefined} | undefined {
+    const [scheme, rstr1] = parseURIScheme(str);
+    if(scheme === undefined) {
+        return undefined;
+    }
+
+    const [authority, port, rstr2] = parseURIAuthority(rstr1);
+    const extidx = rstr2.lastIndexOf(".");
+    if(extidx === -1) {
+        return undefined;
+    }
+    const extension = rstr2.slice(extidx + 1);
+
+    return {
+        scheme: scheme,
+        authority: authority,
+        port: port,
+        path: rstr2,
+        extension: extension
+    };
+}
+
+function parsePackage(jp: any): Package {
+
+}
 
