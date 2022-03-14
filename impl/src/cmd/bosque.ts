d98d4c18b9b83b097724aef2ac1ee9097dcf616d
Mark Marron <marron@microsoft.com>
2022-03-13 23:20:52 -0700

    cmd

--- impl/src/cmd/bosque.ts
+++ impl/src/cmd/bosque.ts
@@ -3,7 +3,7 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-import { Config, ConfigAppTest, ConfigBuild, ConfigFuzz, ConfigRun, ConfigTest, Package, parsePackage, parseURIPath, SourceInfo, URIPath } from "./package_load";
+import { Config, ConfigAppTest, ConfigBuild, ConfigFuzz, ConfigRun, ConfigTest, Package, parsePackage, parseURIPath, URIPath } from "./package_load";
 
 import * as fs from "fs";
 import * as path from "path";
@@ -37,7 +37,7 @@ function generateTestConfigDefault(): ConfigTest {
     };
 }
 
-function generateAppTestConfigDefault(): ConfigTest {
+function generateAppTestConfigDefault(): ConfigAppTest {
     return {
         flavors: ["sym", "icpp", "err"],
         dirs: "*"
@@ -52,44 +52,34 @@ function generateFuzzConfigDefault(): ConfigFuzz {
 
 function help(cmd: CmdTag | undefined) {
     if(cmd === "run" || cmd === undefined) {
-        /*
-        * bosque run [package_path.json] [--entrypoint fname] [--config cname]
-        * bosque run entryfile.bsqapp [--entrypoint fname] --files ...
-        * bosque run [package_path.json] [--entrypoint fname] [--config cname] --args "[...]"
-        * bosque run entryfile.bsqapp [--entrypoint fname] --files ... --args "[...]"
-        */
-        xxxx;
+        process.stdout.write("Run Application:\n");
+        process.stdout.write("bosque run [package_path.json] [--entrypoint fname] [--config cname]\n");
+        process.stdout.write("bosque run [package_path.json] [--entrypoint fname] [--config cname] --args \"[...]\"\n");
+        process.stdout.write("bosque run entryfile.bsqapp [--entrypoint fname] --files ...\n");
+        process.stdout.write("bosque run entryfile.bsqapp [--entrypoint fname] --files ... --args \"[...]\"\n\n");
     }
 
     if(cmd === "build" || cmd === undefined) {
-        /*
-        * bosque build node package_path.json [--config cname] [out]
-        */
-        xxxx;
+        process.stdout.write("Build Application:\n");
+        process.stdout.write("bosque build node package_path.json [--config cname] [out]\n\n");
     }
 
     if(cmd === "test" || cmd === undefined) {
-        /*
-        * bosque test [package_path.json] [--config cname] [--flavors ("sym" | "icpp" | "err" | "chk")] [--dirs ...]
-        * bosque test testfile.bsqtest --files ... [--flavors ("sym" | "icpp" | "err" | "chk")]
-        */
-        xxxx;
+        process.stdout.write("Unit-Test Application:\n");
+        process.stdout.write("bosque test [package_path.json] [--config cname]\n");
+        process.stdout.write("bosque test testfile.bsqtest ... --files ... [--flavors (sym | icpp | err | chk)*]\n\n");
     }
 
     if(cmd === "apptest" || cmd === undefined) {
-        /*
-        * bosque apptest [package_path.json] [--config cname] [--flavors ("sym" | "icpp" | "err" | "chk")] [--dirs ...]
-        * bosque apptest testfile.bsqtest --files ... [--flavors ("sym" | "icpp" | "err" | "chk")]
-        */
-        xxxx;
+        process.stdout.write("EntryPoint Test Application:\n");
+        process.stdout.write("bosque apptest [package_path.json] [--config cname]\n");
+        process.stdout.write("bosque apptest testfile.bsqtest ... --files ... [--flavors (sym | icpp | err | chk)*]\n\n");
     }
 
     if(cmd === "fuzz" || cmd === undefined) {
-        /*
-        * bosque fuzz package_path.json [--config cname]
-        * bosque fuzz package_path.json --files ...
-        */
-        xxxx;
+        process.stdout.write("Fuzz Application:\n");
+        process.stdout.write("bosque fuzz [package_path.json] [--config cname]\n");
+        process.stdout.write("bosque fuzz testfile.bsqapp ... --files ...\n\n");
     }
 }
 
@@ -132,16 +122,25 @@ function tryLoadPackage(pckgpath: string): Package | undefined {
 
 function extractEntryPoint(args: string[]): string | undefined {
     const epidx = args.indexOf("--entrypoint");
-    if(epidx === -1 || epidx === args.length) {
-        return undefined;
+    if(epidx === -1) {
+        return "Main::main";
     }
+    else {
+        if(epidx === args.length - 1) {
+            return undefined;
+        }
 
-    return args[epidx] + 1;
+        return args[epidx] + 1;
+    }
+}
+
+function isStdInArgs(args: string[]): boolean {
+    return args.indexOf("--args") === -1;
 }
 
 function extractArgs(args: string[]): any[] | undefined {
     const argsidx = args.indexOf("--args");
-    if(argsidx === -1 || argsidx === args.length) {
+    if(argsidx === -1 || argsidx === args.length - 1) {
         return undefined;
     }
 
@@ -193,37 +192,199 @@ function extractFiles(workingdir: string, args: string[]): URIPath[] | undefined
     return urifiles;
 }
 
-function extractConfig<T>(args: string[], cmd: CmdTag): Config<T> {
+function extractConfig<T>(args: string[], pckg: Package, workingdir: string, cmd: CmdTag): Config<T> | undefined {
+    const cfgidx = args.indexOf("--confg");
+    if(cfgidx !== -1) {
+        if(cfgidx === args.length - 1) {
+            return undefined;
+        }
 
+        const cfgname = args[cfgidx];
+        if(cmd === "run") {
+            return (pckg.configs.run.find((cfg) => cfg.name === cfgname) as any) as Config<T>;
+        }
+        else if(cmd === "build") {
+            return (pckg.configs.build.find((cfg) => cfg.name === cfgname) as any) as Config<T>;
+        }
+        else if(cmd === "test") {
+            return (pckg.configs.test.find((cfg) => cfg.name === cfgname) as any) as Config<T>;
+        }
+        else if(cmd === "apptest") {
+            return (pckg.configs.apptest.find((cfg) => cfg.name === cfgname) as any) as Config<T>;
+        }
+        else {
+            return (pckg.configs.run.find((cfg) => cfg.name === cfgname) as any) as Config<T>;
+        }
+    }
+    else {
+        if(cmd === "run") {
+            const rcfg = generateRunConfigDefault() as any;
+            return {
+                name: "_run_",
+                macros: [],
+                globalmacros: [],
+                buildlevel: "release",
+                params: rcfg as T
+            };
+        }
+        else if(cmd === "build") {
+            const rcfg = generateBuildConfigDefault(workingdir) as any;
+            return {
+                name: "_build_",
+                macros: [],
+                globalmacros: [],
+                buildlevel: "release",
+                params: rcfg as T
+            };
+        }
+        else if(cmd === "test") {
+            const rcfg = generateTestConfigDefault() as any;
+            return {
+                name: "_test_",
+                macros: [],
+                globalmacros: [],
+                buildlevel: "test",
+                params: rcfg as T
+            };
+        }
+        else if(cmd === "apptest") {
+            const rcfg = generateAppTestConfigDefault() as any;
+            return {
+                name: "_apptest_",
+                macros: [],
+                globalmacros: [],
+                buildlevel: "test",
+                params: rcfg as T
+            };
+        }
+        else {
+            const rcfg = generateFuzzConfigDefault() as any;
+            return {
+                name: "_fuzz_",
+                macros: [],
+                globalmacros: [],
+                buildlevel: "test",
+                params: rcfg as T
+            };
+        }
+    }
 }
 
-function extractSrc<T>(args: string[], cfg: Config<T>): SourceInfo {
-    
+function extractTestFlags(args: string[]): ("sym" | "icpp" | "err" | "chk")[] | undefined {
+    const fidx = args.indexOf("--flavors");
+    if(fidx === -1 || fidx === args.length - 1) {
+        return undefined;
+    }
+
+    let ii = fidx + 1;
+    let flavors: ("sym" | "icpp" | "err" | "chk")[] = [];
+    while(ii < args.length && !args[ii].startsWith("--")) {
+        const flavor = args[ii];
+        if(flavor !== "sym" && flavor !== "icpp" && flavor !== "err" && flavor !== "chk") {
+            return undefined;
+        }
+
+        flavors.push(flavor);
+    } 
+
+    return flavors;
 }
 
-function extractTestFlags(args: string[]): { category: Category[], dirs: string[] } {
+function processRunAction(args: string[]) {
+    if(path.extname(args[0]) === "bsqapp") {
+        const entryfile = args[0];
 
-}
+        const entrypoint = extractEntryPoint(args);
+        if(entrypoint === undefined) {
+            process.stderr.write(chalk.red("Could not parse 'entrypoint' option\n"));
 
-function processRunAction(args: string) {
-    if(path.extname(args[0]) === "json") {
-        const pckg = tryLoadPackage(args[0]);
-        if(pckg === undefined) {
             help("run");
             process.exit(1);
         }
 
-        // bosque run [package_path.json] [--entrypoint fname] [--config cname]
+        let fargs: any[] | undefined = undefined;
+        if (!isStdInArgs(args)) {
+            fargs = extractArgs(args);
+            if (fargs === undefined) {
+                process.stderr.write(chalk.red("Could not parse 'arguments' option\n"));
 
-        // bosque run [package_path.json] [--entrypoint fname] [--config cname] --args "[...]"
-    }
-    else if(path.extname(args[0]) === "bsqapp") {
-        // bosque run entryfile.bsqapp [--entrypoint fname] --files ...
+                help("run");
+                process.exit(1);
+            }
+        }
 
-        // bosque run entryfile.bsqapp [--entrypoint fname] --files ... --args "[...]"
+        const files = extractFiles(process.cwd(), args);
+
+        if(fargs === undefined) {
+            // bosque run entryfile.bsqapp [--entrypoint fname] --files ...
+            xxx;
+        }
+        else {
+            // bosque run entryfile.bsqapp [--entrypoint fname] --files ... --args "[...]"
+            xxx;
+        }
     }
     else {
-        help("run");
-        process.exit(1);
+        let pckg: Package | undefined = undefined;
+        if(path.extname(args[0]) === "json") {
+            pckg = tryLoadPackage(args[0]);
+        }
+        else {
+            const implicitpckg = path.join(process.cwd(), "package.json");
+            if(fs.existsSync(implicitpckg)) {
+                pckg = tryLoadPackage(implicitpckg);
+            }
+        }
+        
+        if(pckg === undefined) {
+            process.stderr.write(chalk.red("Could not parse 'package' option\n"));
+
+            help("run");
+            process.exit(1);
+        }
+
+        const entrypoint = extractEntryPoint(args);
+        if(entrypoint === undefined) {
+            process.stderr.write(chalk.red("Could not parse 'entrypoint' option\n"));
+
+            help("run");
+            process.exit(1);
+        }
+
+        let fargs: any[] | undefined = undefined;
+        if (!isStdInArgs(args)) {
+            fargs = extractArgs(args);
+            if (fargs === undefined) {
+                process.stderr.write(chalk.red("Could not parse 'arguments' option\n"));
+
+                help("run");
+                process.exit(1);
+            }
+        }
+
+        const workingdir = path.dirname(path.normalize(args[0]));
+        let cfg = extractConfig<ConfigRun>(args, pckg, workingdir, "run");
+
+        if(fargs === undefined) {
+            // bosque run [package_path.json] [--entrypoint fname] [--config cname]
+            xxx;
+        }
+        else {
+            // bosque run [package_path.json] [--entrypoint fname] [--config cname] --args "[...]"
+            xxx;
+        }
     }
 }
+
+function processBuildAction(args: string[]) {
+}
+
+function processTestAction(args: string[]) {
+}
+
+function processAppTestAction(args: string[]) {
+}
+
+function processFuzzAction(args: string[]) {
+}
+
