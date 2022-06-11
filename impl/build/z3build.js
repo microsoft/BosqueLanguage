67145620d5b848b636ddb8f00772b470502a5a0c
Mark Marron <marron@microsoft.com>
2022-06-10 22:34:46 -0700

    yep

---
+++ impl/build/z3build.js
@@ -0,0 +1,40 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+const path = require("path");
+const proc = require('child_process');
+
+if(process.argv.length !== 3) {
+    process.stderr.write("");
+    process.exit(1);
+}
+
+let z3dir = process.argv[2];
+let builddir = path.join(z3dir, "build");
+
+let runconfig = "";
+if(process.platform === "darwin") {
+    runconfig = "CXX=clang++ CC=clang FPMATH_ENABLED=False python scripts/mk_make.py --arm64=true --staticlib";
+}
+else if(process.platform === "linux") {
+    runconfig = "CXX=clang++ CC=clang python scripts/mk_make.py --staticlib";
+}
+else {
+    runconfig = "python scripts/mk_make.py --parallel=4";
+}
+
+try {
+    process.stdout.write("Configuring...\n");
+    proc.execSync(runconfig, {cwd: z3dir});
+    
+    process.stdout.write("Building...\n");
+    proc.execSync("make", {cwd: builddir});
+
+    process.stdout.write("Done!\n");
+}
+catch (ex) {
+    console.log(ex.toString());
+    process.exit(1);
+}
