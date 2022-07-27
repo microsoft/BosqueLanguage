75f61ef721ae5b778b14dfb0dab04202efdbca6b
Mark Marron <marron@microsoft.com>
2022-07-27 16:51:26 -0700

    begining

---
+++ impl/src/test/bsqunit/apprunner.ts
@@ -0,0 +1,21 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+/*
+import * as FS from "fs";
+import * as Path from "path";
+
+type ApplicationTest = {
+    dir: string,
+    pass: number,
+    fail: number
+};
+
+const s_application_list = [
+    {dir: "readme_calc", pass: 0, fail: 0},
+    {dir: "tic_tac_toe", pass: 0, fail: 0}
+];
+*/
+
